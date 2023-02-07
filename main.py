3ec9d586fe458bccd96bf0c94f6b5880f0760e3a
Adam Noor <adaminoor@yahoo.com>
2023-02-07 11:19:16 -0500

    Created a function called even_odd_altered that adds a line of code that I think fixes the issue from the book sample.

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 # This is a code created while studying Elements of Programming Interviews in Python.
 
 from five_five_adam import remove_value
+from sample_code_book import even_odd, even_odd_altered
 from six_seven_adam import find_nth_look_say
 from five_nine_adam import all_primes
 
 
 if __name__ == '__main__':
 
-    # Uncomment the functions you would like to run or add your own to test
+# Uncomment the functions that you want to test
 
-    print(all_primes(10000))
+    # print(even_odd([2, 3, 4, 8, 10, 11]))
+    # print(even_odd([2, 3, 4, 6, 7]))
+    print(even_odd_altered([2, 3, 4, 8, 10, 11, 12, 12]))
+    print(even_odd_altered([2, 3, 4, 6, 7]))
+
+# print(all_primes(10000))
 
     # print(find_nth_look_say(15))
     # print(find_nth_look_say(14))
--- sample_code_book.py
+++ sample_code_book.py
@@ -1,5 +1,8 @@
 # doesn't work example from the book
-def even_odd(A: [int]) -> None:
+# I added a line of code that I think fixes it- should we reach out to the author?
+
+
+def even_odd(A: [int]) -> [int]:
     next_even, next_odd = 0, len(A) - 1
 
     while next_even < next_odd:
@@ -7,3 +10,20 @@ def even_odd(A: [int]) -> None:
             next_even += 1
         else:
             A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            print(A[next_even], A[next_odd])
+
+    return A
+
+
+def even_odd_altered(A: [int]) -> [int]:
+    next_even, next_odd = 0, len(A) - 1
+
+    while next_even < next_odd:
+        if A[next_even] % 2 == 0:
+            next_even += 1
+
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            next_odd -= 1
+
+    return A
